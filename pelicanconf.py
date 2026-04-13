1d9fc5c69ac593d7027f286f1447dc52868476ca
HotelsBook <hotelsbook.consultoria@gmail.com>
2026-04-13 08:54:49 -0300

    feat: estrutura inicial da HotelsBook Agency

---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+# HotelsBook Agency Configuration
+AUTHOR = 'HotelsBook Agency'
+SITENAME = 'HotelsBook Agency'
+SITEURL = 'https://hotelsbook.github.io/hotelsbook-agency'
+
+PATH = 'content'
+OUTPUT_PATH = 'output'
+THEME = 'theme'
+TIMEZONE = 'America/Fortaleza'
+DEFAULT_LANG = 'pt'
+MARKUP = ('md',)
+
+# URLs amigáveis
+RELATIVE_URLS = False
+DISPLAY_PAGES_ON_MENU = False
+
+MENUITEMS = [
+    ('Agência', '/agencia.html'),
+    ('Analytics', '/analytics.html'),
+    ('Sobre', '/sobre.html'),
+    ('Parcerias', '/parcerias.html'),
+]
